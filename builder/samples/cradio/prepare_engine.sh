cd927bbf942b5df89d9809314687ad45fba06dc3
Chunlin Li <chunlinl@nvidia.com>
2026-03-01 21:39:40 -0800

    Update license year

--- builder/samples/cradio/prepare_engine.sh
+++ builder/samples/cradio/prepare_engine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Prepare C-RADIOv3-H TensorRT engine: export ONNX and build engine.
